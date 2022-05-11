63a5633c26eae78c89af8a10bd2e3bcee8c17eaf
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-05-11 20:17:05 +0800

    update judge

--- judge.py
+++ judge.py
@@ -9,10 +9,11 @@ while True:
         exit(0)
     if s == "quit":
         break
+    cpu_test_file_name = s[:-1] if s[-1].isdigit() else s
     subprocess.run(['cp', 'headers/{}_data.coe'.format(s), 'headers/data.coe'])
     subprocess.run(['cp', 'headers/{}_text.coe'.format(s), 'headers/text.coe'])
     subprocess.run(['verilator', '-Iheaders', '-Isrc', '--cc',
-                    'src/cpu.v', '--trace', '--exe', 'headers/cpu_{}.cpp'.format(s)])
+                    'src/cpu.v', '--trace', '--exe', 'headers/cpu_{}.cpp'.format(cpu_test_file_name)])
     subprocess.run(['make', '-j', '-C', './obj_dir', '-f', 'Vcpu.mk', 'Vcpu'])
     subprocess.run(['./obj_dir/Vcpu'])
     print('')
