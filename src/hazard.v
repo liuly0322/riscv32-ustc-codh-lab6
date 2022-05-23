417ec3e254af912a7ef0764427be1e2df8c4c390
Liu <liuly0322@mail.ustc.edu.cn>
2022-05-23 15:06:17 +0800

    test: what if something is wrong?

--- src/hazard.v
+++ src/hazard.v
@@ -7,8 +7,11 @@ module hazard(
         output flush_ID
     );
 
-    assign stall_IF = load_use_hazard;
-    assign flush_IF = rstn & pc_change_EX;
-    assign flush_ID = rstn & (pc_change_EX | load_use_hazard);
+//     assign stall_IF = load_use_hazard;
+//     assign flush_IF = rstn & pc_change_EX;
+//     assign flush_ID = rstn & (pc_change_EX | load_use_hazard);
+        assign stall_IF = 0;
+        assign flush_IF = 0;
+        assign flush_ID = 0;
 
 endmodule
